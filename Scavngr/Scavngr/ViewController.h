01c328b5245078a97571bee515935c9ff46f1205
Onno Kluyt <okluyt@constantcontact.com>
2014-08-23 12:41:22 -0700

    Skeletal project.

---
+++ Scavngr/Scavngr/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Scavngr
+//
+//  Created by Kluyt, Onno on 8/23/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
